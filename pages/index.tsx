80c9ad9ce114357a6d7e6544e4ef23afd165b861
RyShoe8 <30275036+RyShoe8@users.noreply.github.com>
2025-04-20 06:56:09 -0500

    fix: move LeagueInfo component to bottom of page

--- pages/index.tsx
+++ pages/index.tsx
@@ -39,9 +39,6 @@ export default function Home() {
 
   return (
     <div className="space-y-6">
-      {/* League Information Component */}
-      {currentLeague && <LeagueInfo />}
-      
       {/* Dashboard Cards Grid */}
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
         {/* Team Overview Card */}
@@ -119,6 +116,9 @@ export default function Home() {
           )}
         </div>
       </div>
+
+      {/* League Information Component */}
+      {currentLeague && <LeagueInfo />}
     </div>
   );
 }
